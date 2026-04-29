87a64fb780874914ed4d353e3f1b969b57965f9a
hualtermello <hualter.mello@escola.pr.gov.br>
2026-04-29 20:22:48 -0300

    Implement client registration with database storage
    Added functionality to save client data to the database.

--- integracao/app.py
+++ integracao/app.py
@@ -1,3 +1,4 @@
+
 #import do flask para criação do servidor
 #render_template para criar uma "ponte" com html
 #request para capturar os dados digitados
@@ -26,5 +27,19 @@ def cadastrar():
   sobrenome = request.form['sobrenome']
   idade = request.form['idade']
   
+  try:
+    conectar =  mysql.connector.connect(**bd_config)
+    cursor = conectar.cursor()
+    queue = "insert into cliente(cpf, primeiro_nome, sobrenome, idade) values (%s, %s, %s, %s)"
+    cursor.execute(queue, (cpf, primeiro_nome, sobrenome, idade))
+    
+    conectar.commit()
+    cursor.close()
+    conectar.close()
+    
+    return f"<h3>Cliente {primeiro_nome} salvo com sucesso!</h3> <a href='/'> Voltar </a>"
+    
+  except mysql.connector.Error as erro:
+    return f"Erro ao gravar no banco: {erro}"
   
-  
+
