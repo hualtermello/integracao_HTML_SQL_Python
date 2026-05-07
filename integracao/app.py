c8514591d1450e0091401087b1ae5f9000ee6851
hualtermello <hualter.mello@escola.pr.gov.br>
2026-05-06 21:59:32 -0300

    Implement delete route for client records
    Added a new route for deleting clients by CPF.

--- integracao/app.py
+++ integracao/app.py
@@ -1,8 +1,7 @@
-
 #import do flask para criação do servidor
 #render_template para criar uma "ponte" com html
 #request para capturar os dados digitados
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, request
 import mysql.connector
 
 #Permite o Flask localizar o caminho dos arquivos
@@ -18,7 +17,7 @@ bd_config = {
 @app.route('/')
 def buscar_index():
   try:
-    #Cria conexão
+    #Cria conexão com MySQL e permite adicionar comando SQL
     conectar = mysql.connector.connect(**bd_config)
     cursor = conectar.cursor(dictionary=True)
 
@@ -31,21 +30,25 @@ def buscar_index():
   except mysql.connector.Error as erro:
     return f"Erro ao carregar a tabela: {erro}"
 
-
+#Cria uma rota para acessar o formulário
 @app.route('/cadastrar', methods=['POST'])
 def cadastrar():
-  #Bloco para armazenar
+  #Bloco para armazenar os dados digitados
   cpf = request.form['cpf']
   primeiro_nome = request.form['primeiro_nome']
   sobrenome = request.form['sobrenome']
   idade = request.form['idade']
   
   try:
+    #verificando conexão com MySQL
     conectar =  mysql.connector.connect(**bd_config)
+    #Variável que permite a escrever SQL
     cursor = conectar.cursor()
+
     queue = "insert into cliente(cpf, primeiro_nome, sobrenome, idade) values (%s, %s, %s, %s)"
     cursor.execute(queue, (cpf, primeiro_nome, sobrenome, idade))
     
+    #Atualiza as alterações e fecha as conexões
     conectar.commit()
     cursor.close()
     conectar.close()
@@ -54,6 +57,24 @@ def cadastrar():
     
   except mysql.connector.Error as erro:
     return f"Erro ao gravar no banco: {erro}"
+
+#Cria a rota para exclusão
+@app.route('/excluir/<cpf>')
+def excluir(cpf):
+    try:
+
+        conectar = mysql.connector.connect(**bd_config)
+        cursor = conectar.cursor()
+
+        cursor.execute("DELETE FROM cliente WHERE CPF = %s", [cpf])
+
+        conectar.commit()
+        cursor.close()
+        conectar.close()
+
+        return redirect(url_for('buscar_index'))
+    except mysql.connector.Error as erro:
+        return f"Erro ao excluir: {erro}"
 
 if __name__ == '__main__':
   app.run(debug=True)
